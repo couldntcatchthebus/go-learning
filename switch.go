70b140d0609657ffe96e5c103f2199c3d4204b18
couldntcatchthebus <ercansavas1@gmail.com>
2021-07-03 17:31:57 +0300

    new files for learning

---
+++ switch.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := "Zurna"
+	switch a {
+	case "Zurna":
+		fmt.Println("Thats true.")
+	case "Bilecik":
+		fmt.Println("Thats wrong.")
+	}
+}
